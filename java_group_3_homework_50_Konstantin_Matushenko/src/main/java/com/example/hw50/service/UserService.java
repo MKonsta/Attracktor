bd7525162f098fc8d8f6a893cc371f22f8c8f0b8
Konstantin Matushenko <kostyan.exe@gmail.com>
2020-03-07 21:18:57 +0600

    add UserService

---
+++ java_group_3_homework_50_Konstantin_Matushenko/src/main/java/com/example/hw50/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.hw50.service;
+
+import com.example.hw50.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public User getUserById(String id);
+
+    public void save(User user);
+
+    public void delete(String id);
+
+    public List<User> getAll();
+}
---
+++ java_group_3_homework_50_Konstantin_Matushenko/src/main/java/com/example/hw50/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.hw50.service;
+
+import com.example.hw50.model.User;
+import com.example.hw50.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void delete(String id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return (List<User>) userRepository.findAll();
+    }
+}
