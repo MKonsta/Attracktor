bbb9e66899b85b66d5c8262d5c40b45584bf2d77
Konstantin Matushenko <kostyan.exe@gmail.com>
2020-03-07 14:57:52 +0600

    First commit

---
+++ java_group_3_homework_50_Konstantin_Matushenko/src/main/java/com/example/hw50/model/User.java
@@ -0,0 +1,88 @@
+package com.example.hw50.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private int publications;
+    private int subsciptions;
+    private int subscibers;
+
+    public User(String name, String email, String password, int publications, int subsciptions, int subscibers) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.publications = publications;
+        this.subsciptions = subsciptions;
+        this.subscibers = subscibers;
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPublications() {
+        return publications;
+    }
+
+    public void setPublications(int publications) {
+        this.publications = publications;
+    }
+
+    public int getSubsciptions() {
+        return subsciptions;
+    }
+
+    public void setSubsciptions(int subsciptions) {
+        this.subsciptions = subsciptions;
+    }
+
+    public int getSubscibers() {
+        return subscibers;
+    }
+
+    public void setSubscibers(int subscibers) {
+        this.subscibers = subscibers;
+    }
+}
---
+++ java_group_3_homework_50_Konstantin_Matushenko/src/main/java/com/example/hw50/model/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.hw50.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, String> {
+
+
+}
