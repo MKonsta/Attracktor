57b117414683af0a211a56ad99dbcb60c0248037
Konstantin Matushenko <kostyan.exe@gmail.com>
2020-03-10 22:47:49 +0600

    hw51 create project

---
+++ java_group_3_homework_51_Konstantin_Matushenko/src/main/java/kg/attracktor/hw51/Hw51Application.java
@@ -0,0 +1,13 @@
+package kg.attracktor.hw51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw51Application.class, args);
+    }
+
+}
---
+++ java_group_3_homework_51_Konstantin_Matushenko/src/test/java/kg/attracktor/hw51/Hw51ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attracktor.hw51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
