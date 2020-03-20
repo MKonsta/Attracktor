8c3332c02c5d05c437d623be117f6e47e20bdc62
Konstantin Matushenko <kostyan.exe@gmail.com>
2020-03-20 22:29:06 +0600

    start hw54

---
+++ java_group_3_homework_54_Konstantin_Matushenko/src/main/java/com/example/hw54/Hw54Application.java
@@ -0,0 +1,13 @@
+package com.example.hw54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw54Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw54Application.class, args);
+    }
+
+}
---
+++ java_group_3_homework_54_Konstantin_Matushenko/src/test/java/com/example/hw54/Hw54ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw54;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw54ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
